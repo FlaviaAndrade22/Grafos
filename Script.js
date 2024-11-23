a62402669c0a9583816a08a637a5278f57b304e1
Flavia Andrade Ribeiro <143115629+FlaviaAndrade22@users.noreply.github.com>
2024-11-22 21:37:25 -0300

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
+function CalcularRota() {
+    // Capturando os valores selecionados
+    const ponto1 = document.getElementById('ponto1').value;
+    const ponto2 = document.getElementById('ponto2').value;
+    const ponto3 = document.getElementById('ponto3').value;
+    const ponto4 = document.getElementById('ponto4').value;
 
+    // Exibindo uma mensagem com a rota escolhida
+    alert(`Rota escolhida: ${ponto1} -> ${ponto2} -> ${ponto3} -> ${ponto4}`);
+}
